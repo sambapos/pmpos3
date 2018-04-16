e4bcbc9d452bde2363724b635b1d89b295f4dc96
Emre Eren <emreeren@gmail.com>
2018-04-16 20:05:55 +0300

    update select card action

--- src/modules/CardOperations/Plugins/SelectCard/component.tsx
+++ src/modules/CardOperations/Plugins/SelectCard/component.tsx
@@ -1,80 +1,31 @@
 import * as React from 'react';
 import { Button } from 'material-ui';
-import DialogContent from 'material-ui/Dialog/DialogContent';
-import DialogTitle from 'material-ui/Dialog/DialogTitle';
-import decorate, { Style } from './style';
-import { WithStyles } from 'material-ui/styles/withStyles';
+import { DialogActions, DialogTitle } from 'material-ui';
 import RuleManager from '../../../RuleManager';
-import CardList from '../../../CardList';
-import TextField from 'material-ui/TextField/TextField';
 import { EditorProps } from '../EditorProps';
+import CardSelectorPage from '../../../../containers/CardSelectorPage';
 
-interface State {
-    cardType: string;
-    buttons: string[];
-    selectedTag: string;
-    searchValue: string;
-}
+type Props = EditorProps<{ type: string, selected: string }>;
 
-type Props = EditorProps<{ type: string, selected: string }> & WithStyles<keyof Style>;
-
-class Component extends React.Component<Props, State> {
-
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            cardType: '',
-            buttons: [],
-            selectedTag: '',
-            searchValue: ''
-        };
-    }
-
-    componentDidMount() {
-        if (this.props.current && this.state.buttons.length === 0) {
-            let ctId = CardList.getCardTypeIdByRef(this.props.current.type);
-            if (ctId) {
-                this.setState({
-                    selectedTag: this.props.current.selected,
-                    cardType: this.props.current.type,
-                    buttons: CardList.getCardsByType(ctId).map(x => x.name).toArray()
-                });
-            }
+export default (props: Props) => {
+    return <>
+        <DialogTitle>{
+            props.current.selected
+                ? `Change ${props.current.type}: ${props.current.selected}`
+                : `Set ${props.current.type}`
         }
-    }
-
-    render() {
-        return <>
-            <DialogTitle>{`Select ${this.state.cardType}`}</DialogTitle>
-            <DialogContent>
-                <TextField
-                    style={{ marginBottom: '8px' }}
-                    fullWidth
-                    label="Search"
-                    value={this.state.searchValue}
-                    onChange={e => this.setState({ searchValue: e.target.value })}
-                />
-                <div className={this.props.classes.buttonContainer}>
-                    {this.state.buttons
-                        .filter(x => !this.state.searchValue || x.includes(this.state.searchValue))
-                        .map(caption => (
-                            <Button
-                                variant="raised"
-                                className={this.props.classes.selectionButton}
-                                color={caption === this.state.selectedTag ? 'secondary' : 'default'}
-                                key={'b_' + caption}
-                                onClick={() => {
-                                    RuleManager.state.set(this.state.cardType, caption);
-                                    this.props.success(this.props.actionName, this.props.current);
-                                }}
-                            >
-                                {caption}
-                            </Button>
-                        ))}
-                </div>
-            </DialogContent>
-        </>;
-    }
-}
-
-export default decorate(Component);
+        </DialogTitle>
+        <div style={{ margin: 4 }}>
+            <CardSelectorPage
+                cardType={props.current.type}
+                onSelectCard={card => {
+                    RuleManager.state.set(props.current.type, card.name);
+                    props.success(props.actionName, props.current);
+                }}
+            />
+        </div>
+        <DialogActions>
+            <Button onClick={() => props.cancel()}>Cancel</Button>
+        </DialogActions>
+    </>;
+};
